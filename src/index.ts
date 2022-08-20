acef129a48283d7d3daa238aec805920be3c8581
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-08-21 00:24:53 +0800

    added class User with methods of get, set, and on

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log('CONNECTED !!!! hello typescript by Parcel !!')
+import { User } from "./models/User";
+
+const bryan = new User({name:"Bryan", age:30});
+bryan.get("name");
+bryan.set({name:"Mr.kouhadi"});
+
+
+bryan.get("name");
