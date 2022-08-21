33d03c768d4c5ce38dac676747935417378623ee
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-08-22 00:05:52 +0800

    added Eventing.ts class

---
+++ src/models/Eventing.ts
@@ -0,0 +1,18 @@
+type Callback = () => void;
+
+export class Eventing{
+    // in on method, we will get events and store them here :
+    events: { [key: string]: Callback[] } = {};
+
+    on(eventName: string, callbackFunc: Callback): void {
+        const arrayOfCallbacks = this.events[eventName] || [];
+        arrayOfCallbacks.push(callbackFunc);
+        this.events[eventName] = arrayOfCallbacks;
+    }
+
+    trigger(eventName: string): void {
+        const handlers = this.events[eventName];
+        if (!handlers || handlers.length === 0) return;
+        handlers.forEach(callbackFunction => callbackFunction());
+    }
+}
