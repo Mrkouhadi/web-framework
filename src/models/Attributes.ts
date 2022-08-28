1d11c3dc863c64bc01d037cb46b8f25badc432a1
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-08-28 14:42:27 +0800

    code refactored

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -3,11 +3,15 @@ export class Attributes<T> {
 
     constructor(private data: T) { }
 
-    get<K extends keyof T>(key: K): T[K] { // means that K can only be one of keys of T
+    get = <K extends keyof T>(key: K): T[K]=> { // means that K can only be one of keys of T
         return this.data[key] // return id/name/age of obj(user) in: new User(obj)
     }
 
     set(update: T): void { // edit obj in: new User(obj)
         Object.assign(this.data, update ) // it's like : this.data = update; override the data;
+    }
+
+    getAll():T{
+        return this.data;
     }
 }   
--- src/models/Eventing.ts
+++ src/models/Eventing.ts
@@ -4,13 +4,13 @@ export class Eventing{
     // in on method, we will get events and store them here :
     events: { [key: string]: Callback[] } = {};
 
-    on(eventName: string, callbackFunc: Callback): void {
+    on = (eventName: string, callbackFunc: Callback): void=> {
         const arrayOfCallbacks = this.events[eventName] || [];
         arrayOfCallbacks.push(callbackFunc);
         this.events[eventName] = arrayOfCallbacks; // store events
     }
 
-    trigger(eventName: string): void {
+    trigger = (eventName: string): void => {
         const handlers = this.events[eventName];
         if (!handlers || handlers.length === 0) return;
         handlers.forEach(callbackFunction => callbackFunction());
