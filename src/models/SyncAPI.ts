9bb2d9c8aab5e27c80ce653b29876afacf6efc77
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-08-28 14:42:05 +0800

    renamed class Sync to SyncApi

---
+++ src/models/SyncAPI.ts
@@ -0,0 +1,24 @@
+import axios, { AxiosPromise } from "axios"
+
+interface HasId{
+    id?:number
+}
+export class SyncApi<T extends HasId>{
+
+    constructor(public rootUrl: string){}
+
+
+    fetch(id:number): AxiosPromise {
+       return axios.get(`${this.rootUrl}/${id}`);
+        // the response will be taken care of when calling fetch(id).then(....)
+    }
+
+    save(data:T):AxiosPromise {
+        const {id} = data; // data.id
+        if(id){
+            return axios.put(`${this.rootUrl}/${id}`, data)
+        }else{
+            return  axios.post(this.rootUrl, data)
+        }            
+    }
+}
