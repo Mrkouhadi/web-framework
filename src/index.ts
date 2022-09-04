465f461b1d27c4616ffab0019950aa83de782e3b
Mr.kouhadi <68134403+Mrkouhadi@users.noreply.github.com>
2022-09-04 13:56:21 +0800

    added url into a sepaet variable

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
+import { UserList } from "./views/UserList";
+
+
+const URL = "http://localhost:3000/users"
+const users = new Collection(URL, (json:UserProps)=>{
+                        return User.buildUser(json)
+                    })
+
+users.on("change", ()=>{
+    const root = document.getElementById("root");
+    if(root) new UserList(root, users).render();
+})
+
+users.fetch()
